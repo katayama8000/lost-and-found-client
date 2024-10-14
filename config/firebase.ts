7b3f2da068502a8ae97f26315203b5c9f56d1d30
katayama8000 <63860688+katayama8000@users.noreply.github.com>
2024-10-14 18:29:57 +0900

    Init (#2)
    * init
    
    * Install biome
    
    * format by biome
    
    * Add settings.json
    
    * add Firebase
    
    * initialize firebase
    
    * formatOnSave

---
+++ config/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyCdcXMgdlh4eJK8D4_KiVl1RHjCApJZ8VE",
+	authDomain: "lost-and-found-3f83f.firebaseapp.com",
+	projectId: "lost-and-found-3f83f",
+	storageBucket: "lost-and-found-3f83f.appspot.com",
+	messagingSenderId: "844445487537",
+	appId: "1:844445487537:web:a8c833490200eb085e2d90",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
